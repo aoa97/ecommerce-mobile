f2c5f7a25034aeccc0de1344a6c6f0f79f6a38e5
aoa97 <eng.ahmedusamma@gmail.com>
2021-07-23 14:29:26 +0200

    API Starter

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,11 @@ import products from './assets/data/productData.js'
 
 const app = express()
 
-// Get Products
+// Get Products 
 app.get('/', (req, res) => {
     res.send(products)
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running ..");
 })
